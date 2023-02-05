79507b47437c8e368e0d91d1a801bfa345f907c1
Shrikrushna Hajare <124458134+dev-krushna@users.noreply.github.com>
2023-02-05 23:58:06 +0530

    Create Variables.java

---
+++ Day2/Variables.java
@@ -0,0 +1,74 @@
+/*
+
+The single line comment begins with a // . It is used in application program.
+
+ MULTILINE :
+
+The Multiline comment begins with a /* and end with a */. 
+  
+  */
+  
+  
+  //Tyes of variabes in Java
+  
+  
+  public class A
+  {
+    static int stat=100; // staitc variable
+    void method()
+    {
+      int number=100; // local variable
+    }
+    
+    public static void main(String args[])
+    {
+      int data=50; // instance variable
+    }
+  }
+
+// Different types of vaiables in java
+
+class var_int{
+  
+  public static void main(String args[]){
+    int num;
+    num=20;
+    System.out.println(num);
+  }
+}
+
+
+
+class var_double{
+    public static void main(String[] args) {
+        double num;
+        num = 10.5;
+        System.out.println(num);
+    }
+}
+
+
+class var_char{
+
+    public static void main(String[] args) {
+        char ch;
+        ch = 'e';
+        System.out.println(ch);
+    }
+}
+
+
+class var_string{
+
+    public static void main(String[] args) {
+        String msg;
+        msg = "Let's learn Java";
+        System.out.println(msg);
+    }
+}
+
+  
+
+    
+    
+ 
