2b001b1aba4ffcbb933f42e25f01d1a7d81e07cb
Shrikrushna Hajare <124458134+dev-krushna@users.noreply.github.com>
2023-02-06 23:45:55 +0530

    Create Datatypes.java

---
+++ Day3/Datatypes.java
@@ -0,0 +1,18 @@
+Class Datatypes
+{
+public static void main(String[] args)
+{
+int a = 10;
+short s = 2;
+byte b = 6;
+long l = 125362133223l;
+float f = 65.20298f;
+double d = 876.765d;
+System.out.println("The integer variable is " + a);
+System.out.println("The short variable is " + s);
+System.out.println("The byte variable is " + b);
+System.out.println("The long variable is " + l);
+System.out.println("The float variable is " + f);
+System.out.println("The double variable is " + d);
+}
+}
