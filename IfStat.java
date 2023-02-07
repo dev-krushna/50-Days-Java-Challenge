f3917c7d75a97286197226c529c0a71ddcb30741
Shrikrushna Hajare <124458134+dev-krushna@users.noreply.github.com>
2023-02-07 23:59:45 +0530

    IfStat.java

---
+++ IfStat.java
@@ -0,0 +1,11 @@
+class IfStat
+{
+  public static void main(String args[])
+    {
+        int age=20;
+        if(age>=18)
+         {
+            System.out.println("You can Vote..!!);
+  }
+  }
+}
