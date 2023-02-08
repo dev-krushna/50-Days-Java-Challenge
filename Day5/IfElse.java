f0b696b268f3deebd3141c02a8d0e9de14535825
Shrikrushna Hajare <124458134+dev-krushna@users.noreply.github.com>
2023-02-08 18:16:57 +0530

    IfElse.java

---
+++ Day5/IfElse.java
@@ -0,0 +1,19 @@
+class IfElse{
+
+	public static void main(String args[]){
+
+	int num1=30;
+	int num2=40;
+
+	if(num1>num2)
+	{
+		System.out.println("Number 1 is greater ... ");
+	}
+	else
+	{
+	 	
+		System.out.println(" Number 2 is greater ...");
+
+	}
+}
+}
