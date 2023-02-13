5f92ad3066b207defed6cca12eea8549510bf34c
Shrikrushna Hajare <124458134+dev-krushna@users.noreply.github.com>
2023-02-13 23:18:21 +0530

    Create IfElseIf.java

---
+++ Day6/IfElseIf.java
@@ -0,0 +1,19 @@
+public class IfElseIf {  
+  public static void main(String[] args) {  
+  String city = "Latur";  
+    if(city == "Pune") {  
+      System.out.println("city is Pune");  }
+    else if (city == "Beed")
+   {  
+      System.out.println("city is Beed");  
+   }
+   else if(city == "Mumbai") 
+   {  
+      System.out.println("city is Mumbai");  
+   }
+   else
+   {  
+      System.out.println(city);  
+    }  
+}  
+}  
