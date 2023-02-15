5fda62ba9fb5c3043b40d7b6d6f8cf01ad19a322
Shrikrushna Hajare <124458134+dev-krushna@users.noreply.github.com>
2023-02-15 20:39:14 +0530

    Create ForLoop.java

---
+++ Day7/ForLoop.java
@@ -0,0 +1,11 @@
+public class ForLoop{    
+  public static void main(String[] args)
+  {    
+  // TODO Auto-generated method stub    
+  String[] names = {"Java","C","C++","Python","JavaScript"};    
+  System.out.println("Printing the content of the array names:\n");    
+  for(String name:names) {    
+  System.out.println(name);    
+  }    
+}    
+}  
