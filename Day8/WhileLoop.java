251c4ac1f5a40e424610e6ad299fadd0c51c713a
Shrikrushna Hajare <124458134+dev-krushna@users.noreply.github.com>
2023-02-16 23:34:19 +0530

    Create WhileLoop.java

---
+++ Day8/WhileLoop.java
@@ -0,0 +1,15 @@
+class WhileLoop{
+  // Program to display numbers from 1 to 5
+
+  public static void main(String[] args) {
+
+    // declare variables
+    int i = 1, n = 5;
+
+    // while loop from 1 to 5
+    while(i <= n) {
+      System.out.println(i);
+      i++;
+    }
+  }
+}
